30da931eb2629be80cfcde40b5315e19832689f6
ikromshi <numanovikrom454@gmail.com>
2023-10-19 12:48:27 -0400

    5 consecutive forks for child/grand child processes

--- labs/lab 4/level1.c
+++ labs/lab 4/level1.c
@@ -4,16 +4,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-        if (fork() == 0){ /* this is child */
-          fprintf(stderr,"Process[%d]:  child in execution....\n",getpid() );
-           sleep(5);
-           fprintf(stderr,"Process[%d]:  child terminating....\n",getpid()  );
-           exit(0);
-}
-        /* Parent code here */
+        pid_t child;
+
+        /* Creating five children */
+        for (int i=0; i<5; i++) {
+                child = fork();
+               
+                /* Child executing */
+                if (child == 0) {
+                        
+                        /* Creating a fork of the child process - grand child of parent*/
+                        pid_t grand_child = fork();
+
+                        /* Grand child executing */
+                        if (grand_child == 0) {
+                                fprintf(stderr, "Process [%d]:  grand child in execution....\n", getpid());
+                                sleep(5);
+                                fprintf(stderr, "Process [%d]:  grand child terminating....\n", getpid());
+                                exit(0);
+                        }
+
+                        /* Rest of child executing */
+                        fprintf(stderr,"Process[%d]:  child in execution....\n",getpid() );
+                        sleep(5);
+                        fprintf(stderr,"Process[%d]:  child terminating....\n",getpid()  );
+                        exit(0);
+                }
+        }
+
+        /* Parant executing */
         printf("Process[%d]: Parent in execution ... \n", getpid() );
         sleep(2);
-        if (wait(NULL) > 0) /* Child terminating */
-                printf("Process[%d]: Parent detects terminating child \n", getpid() );
+
+        /* Waiting for all children to terminate */
+        for (int i=0; i<5; i++) {
+                if (wait(NULL) > 0) {
+                        printf("Process[%d]: Parent detects terminating child \n", getpid() );
+                } 
+        }
+
         printf("Process[%d]: Parent terminating....\n", getpid() );
 }
