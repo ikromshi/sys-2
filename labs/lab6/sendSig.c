b1413ffe0ba00e5776f5d376630c2990a993803f
ikromshi <numanovikrom454@gmail.com>
2023-11-08 17:43:41 -0500

    random sleep time added; level 1 complete

--- labs/lab6/sendSig.c
+++ labs/lab6/sendSig.c
@@ -3,18 +3,22 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
 
 /* Code listing 2.18:
    Using the kill() function to terminate a child process in an infinite loop
  */
 
 int main(){
-  int running_children;
-  
+  int running_children; // number of potential children to fork off;
+  int random_sleep_time; // time, in seconds, for the parent to sleep (0 ≤ t ≤ 2);
+  time_t time_seed; // time that serves as a random seed for the rand() function;
+
   /* Getting the number of children to run */ 
   printf("Enter the number of children to fork off: \n");
   scanf("%d", &running_children);
   
+  /**********************/
   pid_t  PIDs[running_children];
 
 
@@ -33,13 +37,14 @@ int main(){
     /* The child enters an infinite loop */
     if (child_pid == 0) {
       printf("Child process %d is running\n", getpid());
-      // PIDs[i] = child_pid; //getpid();
       while(1);
     }
   }
 
   /* Make the parent sleep so the children get a chance to run */
-  sleep(1);
+  srand((unsigned) time(&time_seed)); // setting the seed to a unique number;
+  random_sleep_time = rand() % 3;
+  sleep(random_sleep_time);
 
   /* The parent sends a SIGKILL signal to kill each child */
   for (int i=0; i<running_children; i++) {
