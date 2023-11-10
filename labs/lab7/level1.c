d4ce94d3ef45d86ab4340d6789a991c35c21e6cf
ikromshi <numanovikrom454@gmail.com>
2023-11-10 16:32:27 -0500

    set up continuous pipe communication between parent and child

--- labs/lab7/level1.c
+++ labs/lab7/level1.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -17,6 +18,8 @@ int p2cfd[2]; /* parent-to-child */
 int c2pfd[2]; /* child-to-parent */
 char buffer[10];
 ssize_t bytes_read = 0;
+time_t time_seed;
+
 
 /* Clear the buffer and open the pipe */
 memset (buffer, 0, sizeof (buffer));
@@ -35,31 +38,64 @@ if (child_pid == 0)
     /* Child closes write end of p2c, read of c2p */
     close (p2cfd[1]);
     close (c2pfd[0]);
-    bytes_read = read (p2cfd[0], buffer, 10);
-    if (bytes_read <= 0)
-      exit (0);
-    printf ("Child received: '%s'\n", buffer);
+    int sum = 0;
 
-    /* Child sends response of "goodbye" */
-    strncpy (buffer, "goodbye", sizeof (buffer));
-    write (c2pfd[1], buffer, 10);
-    exit (0);
+    while (1) {
+      printf("Child running\n");
+      bytes_read = read (p2cfd[0], buffer, 10);
+      if (bytes_read <= 0)
+        exit (0);
+
+      int number = atoi(buffer);
+      sum += number;
+
+      printf ("Child received: the number '%d'\n", number);
+      if (sum < 100) {
+        printf("Current sum: %d\n", sum);
+        strncpy(buffer, "more", sizeof(buffer));
+        write(c2pfd[1], buffer, sizeof(buffer));
+      }
+      else if (sum >= 100) {
+        strncpy(buffer, "goodbye", sizeof(buffer));
+        write(c2pfd[1], buffer, sizeof(buffer));
+        printf("Child exiting\n");
+        exit(0);
+      }
+    }
   }
 
-/* Parent closes read end of p2c, write of c2p */
-close (p2cfd[0]);
-close (c2pfd[1]);
+  /* Parent closes read end of p2c, write of c2p */
+  close (p2cfd[0]);
+  close (c2pfd[1]);
 
-/* Parent sends 'hello' and waits */
-strncpy (buffer, "hello", sizeof (buffer));
-printf ("Parent is sending '%s'\n", buffer);
-write (p2cfd[1], buffer, sizeof (buffer));
+  srand((unsigned) time(&time_seed));
+  int random_number = (rand() % 20) + 1;
 
-/* Parent reads response back from child */
-bytes_read = read (c2pfd[0], buffer, 10);
-if (bytes_read <= 0)
-  exit (1); /* should receive response */
-printf ("Parent received: '%s'\n", buffer);
+  /* Parent sends 'hello' and waits */
+  strncpy (buffer, "10", sizeof (buffer));
+  printf ("Parent is sending '%s'\n", buffer);
+  write (p2cfd[1], buffer, sizeof (buffer));
+  
 
-  return 0;
+  while (1) {
+    /* Parent reads response back from child */
+    bytes_read = read (c2pfd[0], buffer, 10);
+    if (bytes_read <= 0)
+      exit (1); /* should receive response */
+    printf ("Parent received: '%s'\n", buffer);
+
+    printf("Parent running\n");
+    if (strncmp(buffer, "more", sizeof(buffer)) == 0) {
+      printf("Parent received 'more' from child\n");
+      random_number = rand() % 20 + 1;
+      sprintf(buffer, "%d", random_number);
+      printf("Parent sending a random number: %d\n", random_number);
+      write(p2cfd[1], buffer, sizeof(buffer));
+    }
+
+    else if (strncmp(buffer, "goodbye", sizeof(buffer)) == 0) {
+      printf("Parent exiting\n");
+      return 0;
+    }
+  }
 }
