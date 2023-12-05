904cfc904a32e7792221c8889a9b4013f6ea2ca5
ikromshi <numanovikrom454@gmail.com>
2023-12-04 19:12:01 -0500

    level 1 complete

--- labs/lab8/level1.c
+++ labs/lab8/level1.c
@@ -1,5 +1,7 @@
 #include "csapp.h"
 #include "assert.h"
+#include <time.h>
+#include <unistd.h>
 
 /* Code Listing 6.9:
    Passing multiple arguments to a thread requires grouping them into a struct
@@ -7,7 +9,8 @@
 
 struct thread_args {
    int first;
-   const char *second;
+   int second;
+   int sum;
 };
 
 /* Using the convention of casting _args to args */
@@ -15,18 +18,28 @@ void *hello_thread (void *_args)
 {
   /* Cast args into a meaningful pointer type that we can use */
   struct thread_args *args = (struct thread_args *) _args;
-  printf ("First: %d; Second: '%s'\n", args->first, args->second);
+  printf ("First: %d; Second: %d\n", args->first, args->second);
 
+  // updating sum;
+  args->sum = args->first + args->second;
+
+  printf("Returning sum: %d\n", args->sum);
   /* Do not forget to free the struct used for arguments */
-  free (args);
+  // free (args);
   pthread_exit (NULL);
 }
 
 int main(){
+    time_t time_seed;
     pthread_t child;
     struct thread_args *args = malloc (sizeof (struct thread_args));
-    args->first = 5;
-    args->second = "Hello";
+
+    // creating two random numbers;
+    srand((unsigned) time(&time_seed));
+    int random_one = (rand() % 50) + 1;
+    int random_two = (rand() % 50) + 1;
+    args->first = random_one;
+    args->second = random_two;
 
     /* Note that the data structure resides on the heap */
     assert (pthread_create (&child, NULL, hello_thread, args) == 0);
@@ -36,7 +49,11 @@ int main(){
      */
     /* Wait for the thread to finish */
     pthread_join (child, NULL);
+    
+    printf("Received sum value from child for values '%d' and '%d' is: '%d'\n", random_one, random_two, args->sum);
 
+    // free the memory;
+    free(args);
     printf("thread finished, parent %d exiting\n", getpid());
 
 }
